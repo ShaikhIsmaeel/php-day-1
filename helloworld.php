7fc92f5677fc85743831e8594582fb44264bd3e0
ShaikhIsmaeel <ismailshaikh445567@gmail.com>
2025-01-05 16:08:38 +0530

    php day 1

---
+++ helloworld.php
@@ -0,0 +1,23 @@
+<html>
+    <body>
+        <?php
+echo "The man, the myth, the legend";
+echo "  this is a comment example";
+$name = "Kashif";
+$age = 18;
+$time = 2.52;
+$class = true;
+$students_arr = array("Ismail, Wasique, Kashif");
+class student{public $marks;
+}
+$stud = new student();
+$stud->marks = 55;
+$null_var = null;
+echo "<br> string;" ,$name;
+echo "<br> integer;" ,$age;
+echo "<br> float;" ,$time;
+echo "<br> boolean;" ,$class;
+echo "<br> array;" ,$students_arr[0];
+        ?>
+    </body>
+</html>   
